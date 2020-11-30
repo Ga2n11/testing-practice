2704c2b573e93fb3a22ddb70c90a7e9df34c10c8
Gaganpreet Singh <gaganpreetsingh9991@gmail.com>
2020-11-30 15:17:10 -0500

    Updated

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -2,7 +2,22 @@
 // DEFINE FUNCTION(S)
 // ==================================================
 function quotient() {
+  if (x === undefined) {
+        x=1;
+  }
 
+  if (y === undefined) {
+        y=1;
+  }
+
+  if (y === 0) {
+        console.log("Error in division");
+	return 0;
+  }
+
+
+
+   return x/y;
 }
 
 // ==================================================
@@ -20,22 +35,26 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
-
+var result = quotient(2, 0);
+  if (result !== 0) throw new Error('Expected quotient(2, 2) to be 1. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+var result = quotient(2, 2,2);
+  if (result !== 0.5) throw new Error('Expected quotient(2, 2) to be 1. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+var result = quotient(2);
+  if (result !== 2) throw new Error('Expected quotient(2, 2) to be 1. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+var result = quotient();
+  if (result !== 0) throw new Error('Expected quotient(2, 2) to be 1. Received: ' + result);
   console.log('All tests passed successfully.');
 
 // ==================================================
